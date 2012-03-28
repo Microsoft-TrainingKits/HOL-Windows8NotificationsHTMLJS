0162f534522d6d2b3565a1e57fa15b2dda6663ac
Hernan Meydac Jean <hernan.meydac@southworks.net>
2012-03-28 15:47:35 -0300

    - Fixed Setup.cmd

--- Source/Setup.cmd
+++ Source/Setup.cmd
@@ -3,7 +3,7 @@ setlocal
 CD /d "%~dp0"
 
 ::Test If script has Admin Priviledges/is elevated
-REG QUERY "HKUS-1-5-19"
+REG QUERY "HKUS-1-5-19" 2> NUL
 IF %ERRORLEVEL% NEQ 0 (
     ECHO Please run this script as an administrator
     pause
